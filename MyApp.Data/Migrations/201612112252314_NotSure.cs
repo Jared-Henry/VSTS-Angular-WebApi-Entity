c54bb14efa951d69acef5c5dc24069260f2abe97
Jared Henry <jared.klayton.henry@gmail.com>
2016-12-11 15:54:01 -0700

    Useless migration

---
+++ MyApp.Data/Migrations/201612112252314_NotSure.cs
@@ -0,0 +1,16 @@
+namespace MyApp.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NotSure : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
